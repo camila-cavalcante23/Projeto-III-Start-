e6bff21456186512e2f49e9b0d9088b57d9527cf
camila-cavalcante23 <camilacavalcante7405@gmail.com>
2025-07-07 14:24:42 -0300

    cadastro

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -57,90 +57,80 @@ const Register = () => {
 
   return (
     <div>
-      <Navbar2/>
-      <div className="container-register">
-        <div className="side-bar-register">
-          <div></div>
-          <div></div>
-          <p id='ti1' style={{ color: 'white' }}>Bem Vindo</p>
-          <img className='img-logo' src="src/assets/StartUFC-logo-verde.png" alt="Logo" />
-          <p style={{ color: 'white' }}>Ja tem conta na Start?</p>
-          <a href="/login" className='button-btn-login'>Logar</a>
-          
-          <div></div>
-        </div>
-        <div className="register-info">
-          <h1 className="register-title">Crie sua conta</h1>
-          <form onSubmit={handleRegister}>
-            <label htmlFor="full-name" className="register-full-name-label">Nome Completo:</label>
-            <input
-              type="text"
-              className="register-full-name-input"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
-            <br />
-
-            <label htmlFor="tax-number" className="register-tax-number-label">CPF:</label>
-            <input
-              type="text"
-              className="register-tax-number-input"
-              value={cpf}
-              onChange={(e) => setCpf(e.target.value)}
-              required
-            />
-            <br />
-
-            <label htmlFor="email" className="register-email-label">Email:</label>
-            <input
-              type="email"
-              className="register-email-input"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Ex: antonio@gmail.com"
-              required
-            />
-            <br />
-
-            <label htmlFor="phone" className="register-phone-label">Telefone:</label>
-            <input
-              type="tel"
-              className="register-phone-input"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-              placeholder="Ex: (88) 99657 - 5242"
-              required
-            />
-            <br />
-
-            <label htmlFor="password" className="register-password-label">Senha:</label>
-            <input
-              type="password"
-              className="register-password-input"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-            <br />
-
-            <label htmlFor="image" className="register-image-label">Imagem (opcional):</label>
-            <input
-              type="file"
-              className="register-image-input"
-              onChange={(e) => setImage(e.target.files[0])} // Set the image file to state
-            />
-            <br />
-
-            <button type="submit" className="create-account-button" tabIndex="3">Criar Conta</button> 
-          </form>
-
-          {/* {error && <p className="error">{error}</p>}
-          {success && <p className="success">{success}</p>} */}
-          </div>
+    <Navbar2 />
+    <div className="register-page-wrapper">
+        
+        <div className="container-register">
+            <div className="side-bar-register">
+  
+                <p>Bem Vindo</p>
+                <img className='img-logo' src="/src/assets/StartUFC-logo-verde.png" alt="Logo" />
+                <p>Já tem conta na Start?</p>
+                <a href="/login" className='button-btn-login'>Logar</a>
+            </div>
+            <div className="register-info">
+                <h1 className="register-title">Crie sua conta</h1>
+                <form onSubmit={handleRegister}>
+                
+                    <label htmlFor="full-name" className="register-full-name-label">Nome Completo:</label>
+                    <input
+                        type="text"
+                        className="register-full-name-input"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                    />
+                    <label htmlFor="tax-number" className="register-tax-number-label">CPF:</label>
+                    <input
+                        type="text"
+                        className="register-tax-number-input"
+                        value={cpf}
+                        onChange={(e) => setCpf(e.target.value)}
+                        required
+                    />
+                    <label htmlFor="email" className="register-email-label">Email:</label>
+                    <input
+                        type="email"
+                        className="register-email-input"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Ex: antonio@gmail.com"
+                        required
+                    />
+                    <label htmlFor="phone" className="register-phone-label">Telefone:</label>
+                    <input
+                        type="tel"
+                        className="register-phone-input"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                        placeholder="Ex: (88) 99657 - 5242"
+                        required
+                    />
+                    <label htmlFor="password" className="register-password-label">Senha:</label>
+                    <input
+                        type="password"
+                        className="register-password-input"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                    <label htmlFor="image" className="register-image-label">Imagem (opcional):</label>
+                    <input
+                        type="file"
+                        className="register-image-input"
+                        onChange={(e) => setImage(e.target.files[0])}
+                    />
+                    <button type="submit" className="create-account-button" disabled={loading}>
+                        {loading ? 'Criando...' : 'Criar Conta'}
+                    </button> 
+                </form>
+                {error && <p className="register-message register-error-message">{error}</p>}
+                {success && <p className="register-message register-success-message">{success}</p>}
+            </div>
         </div>
     </div>
-  );
+    </div>
+);
 };
 
 export default Register;
