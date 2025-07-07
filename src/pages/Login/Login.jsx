86c7eeba09ac3d75141c1de271995ba3a6368996
camila-cavalcante23 <camilacavalcante7405@gmail.com>
2025-07-07 11:38:03 -0300

    ajustes

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -39,53 +39,51 @@ const Login = () => {
 
   return (
     <div>
-      <Navbar2 />
-      <div className="container-login">
-        <div className="side-bar-login">
-          <div></div>
-          <p style={{ color: 'white' }}>Bem Vindo</p>
-          <img className='img-logo' src="src/assets/StartUFC-logo-verde.png" alt="Logo" />
-          <a style={{ color: 'white' }}>Não tem conta na Start?</a>
-          <a href="/register" className='button_btn_register'>Cadastra-se</a>
-          <div></div>
-        </div>
-        <div className="login-info">
-          <h1 className="login-title">Faça Login</h1>
-          <form onSubmit={handleLogin}>
-            <label htmlFor="email" className="login-email-label">Email:</label>
-            <input 
-              type="email" 
-              className="login-email-input" 
-              placeholder="Ex: antonio@gmail.com" 
-              tabIndex="1" 
-              value={email}
-              onChange={(e) => setEmail(e.target.value)} 
-              required 
-            />
-            <br />
-            <label htmlFor="password" className="login-password-label">Senha:</label>
-            <input 
-              type="password" 
-              className="login-password-input" 
-              tabIndex="2" 
-              value={password}
-              onChange={(e) => setPassword(e.target.value)} 
-              required 
-            />
-            <br />
-            <a tabIndex="4" href="URL" className="reset-password">Esqueceu a senha?</a>
-            <br />
-            <button type="submit" className="button-enter" tabIndex="3" >Entrar</button> 
-          </form>
-          
-          {/* {error && <p style={{ color: 'red' }}>{error}</p>}
-          {success && <p style={{ color: 'green' }}>{success}</p>} */}
+ 
+        <Navbar2 />
 
+        <div className="login-page-wrapper">
+            <div className="container-login">
+                <div className="side-bar-login">
+             
+                    <p>Bem Vindo</p>
+                    <img className='img-logo' src="src/assets/StartUFC-logo-verde.png" alt="Logo" />
+                    <a>Não tem conta <br />na Start?</a>
+                    <a href="/register" className='button_btn_register'>Cadastra-se</a>
+                </div>
+                <div className="login-info">
+              
+                    <h1 className="login-title">Faça Login</h1>
+                    <form onSubmit={handleLogin}>
+                
+                        <label htmlFor="email" className="login-email-label">Email:</label>
+                        <input 
+                            type="email" 
+                            className="login-email-input" 
+                            placeholder="Ex: antonio@gmail.com" 
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)} 
+                            required 
+                        />
+                        <br />
+                        <label htmlFor="password" className="login-password-label">Senha:</label>
+                        <input 
+                            type="password" 
+                            className="login-password-input" 
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)} 
+                            required 
+                        />
+                        <br />
+                        <a href="#" className="reset-password">Esqueceu a senha?</a>
+                        <br />
+                        <button type="submit" className="button-enter">Entrar</button> 
+                    </form>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  );
-};
+);}
 
 export default Login;
 
