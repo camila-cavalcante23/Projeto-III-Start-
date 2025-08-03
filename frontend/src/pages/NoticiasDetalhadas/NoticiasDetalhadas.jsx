1b360971ce1259889616439b813a69308996ab3a
camila-cavalcante23 <camilacavalcante7405@gmail.com>
2025-08-03 15:16:20 -0300

    notícias

--- frontend/src/pages/NoticiasDetalhadas/NoticiasDetalhadas.jsx
+++ frontend/src/pages/NoticiasDetalhadas/NoticiasDetalhadas.jsx
@@ -120,8 +120,8 @@ function NoticiaDetalhadas() {
       <Navbar2 />
       <main className='noticia-detalhada-container'>
         <div className="header-detalhes">
-          <Link to="/noticias" className="back-arrow-detalhes">←</Link>
-          <p>Notícias</p>
+          <Link to="/noticias" className="back-arrow-detalhes">Notícias</Link>
+        
         </div>
         <article className="noticia-article">
           <h1 className="noticia-titulo">{noticia.titulo}</h1>
